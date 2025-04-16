57e08c3b0a40c8a1a904e722093d94b2999a4922
Mama875-collab <chateaumathys@gmail.com>
2025-04-16 12:01:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// Importer les fonctions nécessaires depuis le SDK de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+// Configuration de votre application web Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAzq0LsdEssEyGEKIdFgNsxTP0FGmwZYIU",
+    authDomain: "wifi-24532.firebaseapp.com",
+    projectId: "wifi-24532",
+    storageBucket: "wifi-24532.appspot.com",
+    messagingSenderId: "922684726790",
+    appId: "1:922684726790:web:06fa4ab85441b967daed87",
+    measurementId: "G-MEX7XTF0BG"
+};
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+// Fonction de connexion
+document.getElementById('loginButton').addEventListener('click', login);
+
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Connexion réussie, afficher le contenu protégé
+            document.getElementById("loginForm").style.display = "none";
+            document.getElementById("protectedContent").style.display = "block";
+        })
+        .catch((error) => {
+            alert(error.message); // Afficher le message d'erreur
+        });
+}
